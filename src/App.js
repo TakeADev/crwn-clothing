b941e59a041b439ee86dc936a936ee3b78e61397
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-10-24 11:29:40 -0400

    Create and display categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { v4 as uuidv4 } from 'uuid'
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: uuidv4(),
+      title: 'Hats',
+    },
+    {
+      id: uuidv4(),
+      title: 'Jackets',
+    },
+    {
+      id: uuidv4(),
+      title: 'Sneakers',
+    },
+    {
+      id: uuidv4(),
+      title: 'Womens',
+    },
+    {
+      id: uuidv4(),
+      title: 'Mens',
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='categories-container'>
+        <div className='background-image' />
+        <div className='category-body-container'>
+          {categories.map(({ title, id }) => (
+            <div id={id} key={id}>
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

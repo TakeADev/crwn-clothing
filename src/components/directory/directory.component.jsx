503fff53854462d69292bcfe975636aa059c8ef6
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-10-24 18:47:11 -0400

    Move data back to App component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,38 +1,8 @@
-import { v4 as uuidv4 } from 'uuid'
-
 import './directory.styles.scss'
 
 import CategoryItem from '../category-item/category-item.component'
 
-const categories = [
-  {
-    id: uuidv4(),
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-]
-
-const Directory = () => {
+const Directory = ({ categories }) => {
   return (
     <div>
       <div className='categories-container'>

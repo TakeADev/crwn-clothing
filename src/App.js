204356bfda6badf363f7bc0f2ae35e33fc8ac1a0
TakeADev <chanceconwaydev@gmail.com>
2022-10-26 06:33:45 -0400

    Create Home Route

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import { v4 as uuidv4 } from 'uuid'
-
-import Directory from './components/directory/directory.component'
-
-const categories = [
-  {
-    id: uuidv4(),
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: uuidv4(),
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-]
+import Home from './routes/home/home.component'
+import { Routes, Route, Router } from 'react-router-dom'
 
 const App = () => {
-  return <Directory categories={categories} />
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import Directory from '../../components/directory/directory.component'
+
+const categories = [
+  {
+    id: uuidv4(),
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: uuidv4(),
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: uuidv4(),
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: uuidv4(),
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: uuidv4(),
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+]
+
+const Home = () => {
+  return <Directory categories={categories} />
+}
+
+export default Home

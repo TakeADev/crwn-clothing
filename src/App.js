cdab473c9dc32ef64040bffe8912e72512572565
TakeADev <chanceconwaydev@gmail.com>
2022-10-27 06:47:20 -0400

    Add routes to App component. Change some global styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Home from './routes/home/home.component'
 import { Routes, Route, Router } from 'react-router-dom'
-import Navbar from './components/navbar/navbar.component'
+import Navigation from './routes/navigation/navigation.component'
+import SignIn from './routes/sign-in/sign-in.component'
+
+const Shop = () => {
+  return <h1>This is the shop page</h1>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navbar />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )

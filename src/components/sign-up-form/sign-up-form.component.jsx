47bbfa07850daa2e77c2d1d028c695b180f3db24
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-11-24 09:31:51 -0500

    Style sign up form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,6 +8,8 @@ import {
 
 import FormInput from '../form-input/form-input.component'
 
+import '../sign-up-form/sign-up-form.styles.scss'
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -49,9 +51,10 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <h1>Sign up with your email and password</h1>
         <FormInput
           label={'Display Name: '}
           inputOptions={{

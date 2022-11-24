3316fb3a1b061d854cd519e4bb1cf8325012e8f6
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-11-24 09:05:46 -0500

    Set up firebase util to create document from data from sign-up form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -32,7 +32,10 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInfo = {}
+) => {
   if (!userAuth) return
 
   const userDocRef = doc(db, 'users', userAuth.uid)
@@ -50,6 +53,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInfo,
       })
     } catch (err) {
       alert('Something went wrong' + err)

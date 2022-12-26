4f5cb978798f1490199bb1ebd34b4956a1a70f86
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-12-26 17:40:51 -0500

    Create and style product-card component

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../button/button.component'
+
+import '../product-card/product-card.styles.scss'
+
+export const ProductCard = ({ product }) => {
+  const { imageUrl, name, price } = product
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted'>Add to cart</Button>
+    </div>
+  )
+}

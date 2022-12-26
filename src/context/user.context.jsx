69546cf116410de7ac0644c2d64958f9d992aa48
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-12-26 15:56:16 -0500

    Sets context and creates document with user object

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from 'react'
 import {
+  createUserDocumentFromAuth,
   onAuthStateChangedListener,
-  signOutUser,
 } from '../utils/firebase/firebase.utils'
 
 export const UserContext = createContext({
@@ -13,11 +13,12 @@ export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null)
   const value = { currentUser, setCurrentUser }
 
-  signOutUser()
-
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      console.log(user)
+      if (user) {
+        createUserDocumentFromAuth(user)
+      }
+      setCurrentUser(user)
     })
 
     return unsubscribe

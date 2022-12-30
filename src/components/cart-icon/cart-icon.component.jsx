351daa62f2fe65563b81589cb673af8288a696df
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-12-30 17:23:40 -0500

    Create and style cart icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
-export const CartIcon = () => {
-  return <div>CartIcon</div>
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+
+import '../cart-icon/cart-icon.styles.scss'
+
+export const CartIcon = ({ onClick }) => {
+  return (
+    <div className='cart-icon-container' onClick={onClick}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>0</span>
+    </div>
+  )
 }

14c1f1722d33434b8ce2b7ce883739dadfb6073f
TakeADev <106417346+TakeADev@users.noreply.github.com>
2022-12-30 17:24:23 -0500

    Add cart icon and dropdown to navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Fragment, useContext } from 'react'
+import { Fragment, useContext, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
@@ -9,12 +9,21 @@ import './navigation.styles.scss'
 import { UserContext } from '../../context/user.context'
 
 import { signOutUser } from '../../utils/firebase/firebase.utils'
+import { CartIcon } from '../../components/cart-icon/cart-icon.component'
+import { CartDropdown } from '../../components/cart-dropdown/cart-dropdown.component'
+import { CartContext } from '../../context/cart.context'
 
 export const Navigation = () => {
   const { currentUser } = useContext(UserContext)
+  const { cartIsOpen, setCartIsOpen } = useContext(CartContext)
 
   const signOutHandler = async () => {
     await signOutUser()
+  }
+
+  const cartClickHandler = (e) => {
+    e.preventDefault()
+    cartIsOpen ? setCartIsOpen(false) : setCartIsOpen(true)
   }
 
   return (
@@ -36,7 +45,9 @@ export const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon onClick={cartClickHandler} />
         </div>
+        {cartIsOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>

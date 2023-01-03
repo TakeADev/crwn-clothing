688746d2ec1097e73b7c91b9aa9b81d91f9f4309
TakeADev <106417346+TakeADev@users.noreply.github.com>
2023-01-03 14:52:36 -0500

    Products can be added to cart

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+
+import { useContext } from 'react'
+
 import { Button } from '../button/button.component'
+
+import { CartContext } from '../../context/cart.context'
 
 import '../product-card/product-card.styles.scss'
 
 export const ProductCard = ({ product }) => {
   const { imageUrl, name, price } = product
+
+  const { addItemToCart } = useContext(CartContext)
+
+  const addProductToCart = () => addItemToCart(product)
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`} />
@@ -12,7 +21,9 @@ export const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to cart</Button>
+      <Button buttonType='inverted' onClick={addProductToCart}>
+        Add to cart
+      </Button>
     </div>
   )
 }

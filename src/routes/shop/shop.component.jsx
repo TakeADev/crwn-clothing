267be9ae12a13e3968a0a60a49fbb4480119ceed
TakeADev <106417346+TakeADev@users.noreply.github.com>
2023-01-19 20:21:57 -0500

    Include categories preview and setup routing

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 
-import { useContext, Fragment } from 'react'
-import { CategoryPreview } from '../../components/category-preview/category-preview.component'
-
-import { CategoriesContext } from '../../context/categories.context'
+import { Routes, Route } from 'react-router-dom'
+import { CategoriesPreview } from '../../routes/categories-preview/categories-preview.component'
 
 import '../shop/shop-styles.scss'
 
 export const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext)
-
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title]
-        return <CategoryPreview key={title} title={title} products={products} />
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   )
 }

701060e5293ef0f13026116e311f75d324fd2d6c
TakeADev <106417346+TakeADev@users.noreply.github.com>
2023-01-19 19:58:58 -0500

    Create structure for category preview component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
+import { ProductCard } from '../product-card/product-card.component'
+
 import '../category-preview/category-preview.styles.scss'
 
 export const CategoryPreview = ({ title, products }) => {
   return (
-    <div className='category-preview-containser'>
+    <div className='category-preview-container'>
       <h2>
-        <span>{title.toUpperCase()}</span>
+        <span className='title'>{title.toUpperCase()}</span>
       </h2>
+      <div className='preview'>
+        {products
+          .filter((_, i) => i < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
     </div>
   )
 }

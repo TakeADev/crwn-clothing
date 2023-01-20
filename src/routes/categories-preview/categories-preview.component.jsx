3e18458dc2c0ccbb3bb374c6fb13efdd51330a30
TakeADev <106417346+TakeADev@users.noreply.github.com>
2023-01-19 20:23:04 -0500

    Create categories preview component

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { useContext, Fragment } from 'react'
+import { CategoryPreview } from '../../components/category-preview/category-preview.component'
+
+import { CategoriesContext } from '../../context/categories.context'
+
+export const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext)
+
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title]
+        return <CategoryPreview key={title} title={title} products={products} />
+      })}
+    </Fragment>
+  )
+}

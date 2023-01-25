4386e5964c263e3bd0645c07971515a63195f3d9
TakeADev <106417346+TakeADev@users.noreply.github.com>
2023-01-25 13:12:41 -0500

    Create category component and add category route

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Routes, Route } from 'react-router-dom'
 import { CategoriesPreview } from '../../routes/categories-preview/categories-preview.component'
+import { Category } from '../category/category.component'
 
 import '../shop/shop-styles.scss'
 
@@ -9,6 +10,7 @@ export const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
     </Routes>
   )
 }

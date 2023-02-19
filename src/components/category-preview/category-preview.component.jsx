16e32d01b6f923e591e2709d6857759eee0a7f79
Takeadev <chanceconwaydev@gmail.com>
2023-02-19 14:22:15 -0500

    Change category-preview styles to styled component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -3,23 +3,27 @@ import { Link } from 'react-router-dom'
 
 import { ProductCard } from '../product-card/product-card.component'
 
-import '../category-preview/category-preview.styles.scss'
+import {
+  CategoryPreviewContainer,
+  CategoryPreviewTitle,
+  CategoryPreviewPreviews,
+} from './category-preview.styles'
 
 export const CategoryPreview = ({ title, products }) => {
   return (
-    <div className='category-preview-container'>
+    <CategoryPreviewContainer>
       <h2>
         <Link to={`/shop/${title}`}>
-          <span className='title'>{title.toUpperCase()}</span>
+          <CategoryPreviewTitle>{title.toUpperCase()}</CategoryPreviewTitle>
         </Link>
       </h2>
-      <div className='preview'>
+      <CategoryPreviewPreviews>
         {products
           .filter((_, i) => i < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </CategoryPreviewPreviews>
+    </CategoryPreviewContainer>
   )
 }

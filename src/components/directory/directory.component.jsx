4bca7d8a25776abc4060c61a2f526910841344bf
Takeadev <chanceconwaydev@gmail.com>
2023-02-19 14:49:15 -0500

    Change directory styles to styled component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,15 +1,13 @@
-import './directory.styles.scss'
+import { DirectoryContainer } from './directory.styles'
 
 import { DirectoryItem } from '../directory-item/directory-item.component'
 
 export const Directory = ({ categories }) => {
   return (
-    <div>
-      <div className='directory-container'>
-        {categories.map((category) => (
-          <DirectoryItem category={category} key={category.id} />
-        ))}
-      </div>
-    </div>
+    <DirectoryContainer>
+      {categories.map((category) => (
+        <DirectoryItem category={category} key={category.id} />
+      ))}
+    </DirectoryContainer>
   )
 }

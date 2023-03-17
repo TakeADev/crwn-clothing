769724724e7a3b80326ff9d0d7392b8e168176d2
TakeADev <chanceconwaydev@gmail.com>
2023-03-17 17:31:57 -0400

    Change sign up form styles to styled component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -9,7 +9,7 @@ import {
 import { FormInput } from '../form-input/form-input.component'
 import { Button } from '../button/button.component'
 
-import '../sign-up-form/sign-up-form.styles.scss'
+import { SignUpContainer } from './sign-up-form.styles'
 
 const defaultFormFields = {
   displayName: '',
@@ -60,7 +60,7 @@ export const SignUpForm = () => {
   }
 
   return (
-    <div className='sign-up-container'>
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -106,6 +106,6 @@ export const SignUpForm = () => {
         />
         <Button type='submit'>Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   )
 }

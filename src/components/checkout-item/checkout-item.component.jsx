54fa74ebd2bb5b4f500db43d69676feb87e7c712
TakeADev <chanceconwaydev@gmail.com>
2023-03-17 17:11:50 -0400

    Change checkout item styles to styled component.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { useContext } from 'react'
 import { CartContext } from '../../context/cart.context'
-import { Button } from '../button/button.component'
 
-import '../checkout-item/checkout-item.styles.scss'
+import {
+  CheckoutItemContainer,
+  CheckoutItemImageContainer,
+  Price,
+  Quanitiy,
+  Arrow,
+  Value,
+  RemoveButton,
+} from './checkout-item.styles.jsx'
 
 const CheckoutItem = ({ checkoutItem }) => {
   const { name, imageUrl, price, quantity } = checkoutItem
@@ -16,23 +23,19 @@ const CheckoutItem = ({ checkoutItem }) => {
   const clearCartItem = () => clearItemFromCart(checkoutItem)
 
   return (
-    <div className='checkout-item-container'>
-      <img className='image-container' src={imageUrl} alt={name} />
-      <span className='name'>{name}</span>
-      <div className='quantity'>
-        <div className='arrow' onClick={removeCartItem}>
-          &#10094;
-        </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={increaseQuantity}>
-          &#10095;
-        </div>
-      </div>
-      <span className='price'>${price}</span>
-      <div className='remove-button' onClick={clearCartItem}>
-        &#10005;
-      </div>
-    </div>
+    <CheckoutItemContainer>
+      <CheckoutItemImageContainer alt={name}>
+        <img src={imageUrl} alt={name} />
+      </CheckoutItemImageContainer>
+      <span>{name}</span>
+      <Quanitiy>
+        <Arrow onClick={removeCartItem}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={increaseQuantity}>&#10095;</Arrow>
+      </Quanitiy>
+      <Price>${price}</Price>
+      <RemoveButton onClick={clearCartItem}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   )
 }
 

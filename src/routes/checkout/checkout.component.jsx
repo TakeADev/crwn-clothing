4eff980293ff72c6e5916c1ab3e5fe94eced5235
TakeADev <chanceconwaydev@gmail.com>
2023-03-17 17:43:05 -0400

    Change checkout styles to styled component.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,36 +3,41 @@ import { useContext } from 'react'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { CartContext } from '../../context/cart.context'
 
-import '../checkout/checkout.styles.scss'
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from './checkout.styles.jsx'
 
 export const Checkout = () => {
   const { cartItems, cartTotal } = useContext(CartContext)
 
   return (
-    <div className='checkout-container'>
-      <div className='checkout-header'>
-        <div className='header-block'>
+    <CheckoutContainer>
+      <CheckoutHeader>
+        <HeaderBlock>
           <span>Product</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Description</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Quantity</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Price</span>
-        </div>
-        <div className='header-block'>
+        </HeaderBlock>
+        <HeaderBlock>
           <span>Remove</span>
-        </div>
-      </div>
+        </HeaderBlock>
+      </CheckoutHeader>
 
       {cartItems &&
         cartItems.map((item) => {
           return <CheckoutItem key={item.id} checkoutItem={item} />
         })}
-      <span className='total'>Total: ${cartTotal}</span>
-    </div>
+      <Total>Total: ${cartTotal}</Total>
+    </CheckoutContainer>
   )
 }

bfebde6e770b36d0df13edb93c7931254443c4fd
TakeADev <chanceconwaydev@gmail.com>
2023-07-18 10:20:28 -0400

    Remove user context provider

--- src/index.js
+++ src/index.js
@@ -7,22 +7,20 @@ import { Provider } from 'react-redux'
 import { store } from './store/store'
 
 import { App } from './App'
-import { UserProvider } from './context/user.context'
 import { CategoriesProvider } from './context/categories.context'
 import { CartProvider } from './context/cart.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
